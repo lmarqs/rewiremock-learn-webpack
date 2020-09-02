b8cb15a85b927835853956475d281d67cb6c18ae
lmarqs <marques.analista@gmail.com>
2020-09-02 18:20:27 -0300

    add rewiremock base settings

--- webpack.config.js
+++ webpack.config.js
@@ -7,3 +7,17 @@ module.exports = {
     entry,
     plugins,
 }
+
+if (process.argv.includes('--mode=development')) {
+    entry.unshift('./support/rewiremock.js');
+    
+    const { NamedModulesPlugin, HotModuleReplacementPlugin } = require('webpack');
+
+    const RewireMockPlugin = require('rewiremock/webpack/plugin');
+
+    plugins.push(
+        new NamedModulesPlugin(),
+        new HotModuleReplacementPlugin(),
+        new RewireMockPlugin(),
+    );
+}
