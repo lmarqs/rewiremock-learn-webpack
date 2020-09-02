37e1838124fcb57059c571cd74b29688e01acc9f
lmarqs <marques.analista@gmail.com>
2020-09-02 18:20:44 -0300

    implement mock

--- support/rewiremock.js
+++ support/rewiremock.js
@@ -1,3 +1,10 @@
 import rewiremock from 'rewiremock/webpack';
+import sum from '../src/sum'
+
+let counter = 0;
+rewiremock('./src/sum.js').withDefault((...args) => {
+    console.log(`sum calls: ${++counter}`);
+    return sum(...args);
+});
 
 rewiremock.enable();
